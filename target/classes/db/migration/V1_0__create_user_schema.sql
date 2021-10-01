88992b977b06f4bb39b9cce8c357ae08074b74c7
Maria M <73521257+mariamura@users.noreply.github.com>
2021-10-01 19:06:27 +0300

    Delete target directory

--- target/classes/db/migration/V1_0__create_user_schema.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE if not exists users (
-     id                 int NOT NULL AUTO_INCREMENT,
-     userName           CHAR(30) NOT NULL,
-     PRIMARY KEY (id)
-);
--- target/classes/db/migration/V2_0__create_event_schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE if not exists event (
-     id                 int NOT NULL AUTO_INCREMENT,
-     date               DATE NOT NULL,
-     PRIMARY KEY (id)
-);
-
-
